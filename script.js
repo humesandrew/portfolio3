b2850f46d2f84d1e9e1e3fde7bb41d797656ec5f
Andy Humes <andrew.humes@du.edu>
2022-04-03 20:40:18 -0600

    updating bio section static

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 // creating the biocard on click //////////////////////////////////
 
-const bioCard = document.getElementById("card1");
-bioCard.onclick = function displayBioCard() {
-  //   bioCard.style.backgroundImage = "none";
-  bioCard.style.color = "white";
-  bioCard.style.fontSize = "30px";
-  bioCard.textContent =
-    "I am an aspiring coder. On this page, you will see some examples of my work, my resume, and contact info. I am also part of the leadership for the Colorado chapter of the Foundation Fighting Blindness.";
-  bioCard.setAttribute("data-state", "clicked");
-  bioCard.style.textAlign = "center";
-  var section = document.createElement("hr");
-  bioCard.appendChild(section);
+// const bioCard = document.getElementById("card1");
+// bioCard.onclick = function displayBioCard() {
+//   //   bioCard.style.backgroundImage = "none";
+//   bioCard.style.color = "white";
+//   bioCard.style.fontSize = "30px";
+//   bioCard.textContent =
+//     "I am an aspiring coder. On this page, you will see some examples of my work, my resume, and contact info. I am also part of the leadership for the Colorado chapter of the Foundation Fighting Blindness.";
+//   bioCard.setAttribute("data-state", "clicked");
+//   bioCard.style.textAlign = "center";
+//   var section = document.createElement("hr");
+//   bioCard.appendChild(section);
 
-  var para = document.createElement("p");
-  var node = document.createTextNode(
-    "If you are looking to connect with the Foundation, please see my contact section."
-  );
-  para.appendChild(node);
-  bioCard.appendChild(para);
-};
+//   var para = document.createElement("p");
+//   var node = document.createTextNode(
+//     "If you are looking to connect with the Foundation, please see my contact section."
+//   );
+//   para.appendChild(node);
+//   bioCard.appendChild(para);
+// };
 
 /// creating the portfolio card on click///
 
